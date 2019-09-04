4cc007c20c3f0a34912121114bdb9bec3e8f804d
JorritKornelis <33859036+JorritKornelis@users.noreply.github.com>
2019-09-04 14:39:00 +0200

    basic movemt but need navemesh

---
+++ Periode-9/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public Transform target;
+
+    void Update()
+    {
+        //AiMovement();
+    }
+
+    public void AiMovement()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = target.position;
+    }
+}
