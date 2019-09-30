586bdd3db35e744790ac159d2fa8e499ff4d3fee
JelmerBatenburg <jelmer.batenburg@gmail.com>
2019-09-30 09:40:54 +0200

    CharacterController

---
+++ Periode-9/Assets/Scripts/PlayerAndEnnemy/CameraFocus.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFocus : MonoBehaviour
+{
+    public CameraPositions originalPos;
+    public List<CameraPositions> positions;
+    public float lerpSpeed;
+    public int current = -1;
+}
+
+[System.Serializable]
+public class CameraPositions
+{
+    public Vector3 pos;
+    public float fOV;
+}
