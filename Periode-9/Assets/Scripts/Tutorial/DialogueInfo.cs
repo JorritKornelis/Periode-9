5296f84f6aac6adee0a6dd15993142efc6cca5bf
JelmerBatenburg <jelmer.batenburg@gmail.com>
2019-10-15 11:30:39 +0200

    vergeten te pushen

---
+++ Periode-9/Assets/Scripts/Tutorial/DialogueInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DialogueInfo", menuName = "DialogueInfo")]
+public class DialogueInfo : ScriptableObject
+{
+    public DialoguePartInfo[] dialogue;
+}
+
+[System.Serializable]
+public class DialoguePartInfo
+{
+    public bool enthusiastic;
+    public string message;
+}
---
+++ Periode-9/Assets/Scripts/Tutorial/DialogueSystem.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueSystem : MonoBehaviour
+{
+    public AnimationClip[] normalAnimations;
+    public AnimationClip[] enthusiasticAnimations;
+    public Text textInput;
+    public GameObject uiPanel;
+
+    public void StartDialogue(DialogueInfo info)
+    {
+        uiPanel.SetActive(true);
+    }
+}
