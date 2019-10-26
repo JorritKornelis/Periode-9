57caf643199df7163c6592826179d5a4faed1654
JorritKornelis <33859036+JorritKornelis@users.noreply.github.com>
2019-10-26 16:51:56 +0200

    start ingame options

---
+++ Periode-9/Assets/Scripts/OptionsAndMenu/OptionsMenuInGame.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsMenuInGame : MonoBehaviour
+{
+    public GameObject optionsPanel;
+    bool open = false;
+
+    void Awake()
+    {
+        optionsPanel.SetActive(false);
+    }
+
+    void Update()
+    {
+        OpenOptionsInGameFunction();
+    }
+
+    public void OpenOptionsInGameFunction()
+    {
+        open = true;
+        if (Input.GetButton("Escape") && open == true)
+        {
+            optionsPanel.SetActive(!optionsPanel.activeSelf);
+            open = false;
+        }
+    }
+
+}
