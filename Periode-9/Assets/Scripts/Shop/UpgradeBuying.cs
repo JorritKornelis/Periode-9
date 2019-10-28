7ce15fec9576c7579bff5e88fe77dfd02416a44c
JelmerBatenburg <jelmer.batenburg@gmail.com>
2019-10-28 13:59:38 +0100

    Working on upgrades

---
+++ Periode-9/Assets/Scripts/Shop/UpgradeBuying.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeBuying : ShopAcessScript
+{
+    public GameObject upgradeUI;
+    public DialogueSystem dialogue;
+    public DialogueInfo[] dialogues;
+
+    public override void Interact()
+    {
+        Inventory inv = GameObject.FindWithTag("Player").GetComponent<Inventory>();
+        if (active)
+        {
+            active = false;
+            upgradeUI.SetActive(false);
+            inv.inv.SetActive(false);
+        }
+        else
+        {
+            inv.inv.SetActive(false);
+            active = true;
+            StartCoroutine(dialogue.StartDialogue(dialogues[Random.Range(0, dialogues.Length)]));
+        }
+    }
+}
