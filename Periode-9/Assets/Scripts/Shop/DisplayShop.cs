4811876bd9bddbe126c0a8066d88518d61e35979
JelmerBatenburg <jelmer.batenburg@gmail.com>
2019-10-29 17:07:07 +0100

    ShopVisualChange

---
+++ Periode-9/Assets/Scripts/Shop/DisplayShop.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayShop : MonoBehaviour
+{
+    public string savingTag;
+    public Saving saving;
+    public GameObject shop1, shop2;
+
+    public void Start()
+    {
+        saving = GameObject.FindWithTag(savingTag).GetComponent<Saving>();
+        UpdateVisual();
+    }
+
+    public void UpdateVisual()
+    {
+        shop1.SetActive((saving.data.unlocks.isUpgradeShop == 0) ? true : false);
+        shop2.SetActive((saving.data.unlocks.isUpgradeShop == 1) ? true : false);
+    }
+}
