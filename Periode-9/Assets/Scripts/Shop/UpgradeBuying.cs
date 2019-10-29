bed1920add3cfb13799290ddecca13ff7e438e2d
JelmerBatenburg <jelmer.batenburg@gmail.com>
2019-10-29 13:14:33 +0100

    Meta

--- Periode-9/Assets/Scripts/Shop/UpgradeBuying.cs
+++ Periode-9/Assets/Scripts/Shop/UpgradeBuying.cs
@@ -1,12 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UpgradeBuying : ShopAcessScript
 {
     public GameObject upgradeUI;
     public DialogueSystem dialogue;
     public DialogueInfo[] dialogues;
+    public Saving saving;
+    public string savingTag;
+
+    public UpgradeSlot[] shopUpgrades;
+
+    public void Start()
+    {
+        saving = GameObject.FindWithTag(savingTag).GetComponent<Saving>();
+        DisplayUpgrades();
+    }
 
     public override void Interact()
     {
@@ -24,4 +35,34 @@ public class UpgradeBuying : ShopAcessScript
             StartCoroutine(dialogue.StartDialogue(dialogues[Random.Range(0, dialogues.Length)]));
         }
     }
+
+    public void DisplayUpgrades()
+    {
+        for (int i = 0; i < shopUpgrades.Length; i++)
+        {
+            shopUpgrades[i].priceDisplay.text = shopUpgrades[i].price.ToString();
+            if (i <= saving.data.unlocks.isUpgradeShop)
+            {
+                shopUpgrades[i].priceDisplay.gameObject.SetActive(false);
+                if (i == saving.data.unlocks.isUpgradeShop)
+                    shopUpgrades[i].selectedObject.SetActive(true);
+                else
+                    shopUpgrades[i].selectedObject.SetActive(false);
+            }
+            else
+            {
+                shopUpgrades[i].priceDisplay.gameObject.SetActive(true);
+                shopUpgrades[i].selectedObject.SetActive(false);
+            }
+
+        }
+    }
+}
+
+[System.Serializable]
+public class UpgradeSlot
+{
+    public int price;
+    public GameObject selectedObject;
+    public Text priceDisplay;
 }
